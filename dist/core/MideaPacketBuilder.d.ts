31d4e56371486926dd6003c08f3bbbf84cedeabe
bwdiehl <110100894+Ben-Diehlci@users.noreply.github.com>
2026-04-17 16:34:03 -0400

    Revert dist/ and .gitignore to original state

--- dist/core/MideaPacketBuilder.d.ts
+++
@@ -1,9 +0,0 @@
-export default class PacketBuilder {
-    private readonly command;
-    private readonly security;
-    private packet;
-    constructor(device_id: number, command: Buffer);
-    finalize(message_type?: number): Buffer;
-    static packet_time(): Buffer;
-    static checksum(data: Buffer): number;
-}
