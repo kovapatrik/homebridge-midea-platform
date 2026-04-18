9d428cadf24b4b017f7be45a9eaef20b9214df9b
bwdiehl <110100894+Ben-Diehlci@users.noreply.github.com>
2026-04-17 21:58:27 -0400

    Remove dist/ from repo

--- dist/index.d.ts
+++
@@ -1,6 +0,0 @@
-import type { API } from 'homebridge';
-/**
- * This method registers the platform with Homebridge
- */
-declare const _default: (api: API) => void;
-export default _default;
--- dist/index.js
+++
@@ -1,9 +0,0 @@
-import { MideaPlatform } from './platform.js';
-import { PLATFORM_NAME } from './settings.js';
-/**
- * This method registers the platform with Homebridge
- */
-export default (api) => {
-    api.registerPlatform(PLATFORM_NAME, MideaPlatform);
-};
-//# sourceMappingURL=index.js.map
